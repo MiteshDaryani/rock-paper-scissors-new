54c88d1be0309387cd360c7430e8612399986c1f
Mitesh Daryani <miteshdaryani.29@gmail.com>
2023-05-19 00:06:02 +0530

    Made a function play round which declares the winner and loser

--- main.js
+++ main.js
@@ -5,4 +5,47 @@ function getComputerChoice(){
     return result;   
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+
+  if(playerSelection == computerSelection){
+
+    return "It's a tie"
+ }
+
+ else if(playerSelection == 'rock'){
+     if(computerSelection == 'scissors'){
+         return `You Win! ${playerSelection} beats ${computerSelection}`;
+     }
+
+     else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+     }
+ }
+
+ else if(playerSelection == 'paper'){
+     if(computerSelection == 'rock'){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+     }
+
+     else{
+
+         return `You Lose! ${computerSelection} beats ${playerSelection}`
+     }
+ }
+
+ else if(playerSelection == 'scissors'){
+     if(computerSelection == 'paper'){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+     }
+
+     else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+     }
+ }
+ 
+}
+ 
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
